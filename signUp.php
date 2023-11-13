3106a80d9af3db088e40318f216178d9102de4d7
Wike Laelatunuji <wikelaela22@gmail.com>
2023-11-13 13:52:27 +0700

    ok

--- signUp.php
+++ signUp.php
@@ -18,39 +18,55 @@ if (isset($_POST['signUp'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
-</head>
-
-<body>
-    <form action="" method="post">
-        <!-- First Name -->
-        <label for="firstName">First Name</label>
-        <input type="text" name="firstName" id="firstName" required autocomplete="off">
-
-        <!-- Last Name -->
-        <label for="lastName">Last Name</label>
-        <input type="text" name="lastName" id="lastName" required autocomplete="off">
-
-        <!-- Email -->
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email" required autocomplete="off">
-
-        <!-- Password -->
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
-
-        <!-- Confirm Password -->
-        <label for="confPassword">Confirm Password</label>
-        <input type="password" name="confPassword" id="confPassword" required>
-
-        <button type="submit" name="signUp">Sign Up</button> <br><br>
-
-        <a href="signIn.php">Sign In</a>
-    </form>
-</body>
-
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>Document</title>
+        <style>
+            body {
+                background: linear-gradient(to top right, #474e9c, #e387b8);
+            }
+            input {
+                background-color: transparent;
+            }
+            input::placeholder {
+                color: white !important;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex text-white">
+            <div class="w-[50%]">
+                <div class="w-[60%] p-4 mx-auto mt-16 flex flex-col">
+                    <img src="disney-logo-white.png" alt="" class="h-16 object-contain mt-4" />
+                    <div class="font-medium text-white text-center text-4xl mb-4">Sign Up to Your Account</div>
+                    <div class="flex justify-between">
+                        <div class="w-[50%]">
+                            <p class="font-bold text-xl mb-2">First Name</p>
+                            <input type="text" class="border-[2px] border-white rounded-lg h-12 ps-2" name="firstName" placeholder="Input your first name" />
+                        </div>
+                        <div class="w-[50%]">
+                            <p class="font-bold text-xl mb-2">Last Name</p>
+                            <input type="text" class="border-[2px] border-white rounded-lg h-12 ps-2 w-full" name="lastName" placeholder="Input your last name" />
+                        </div>
+                    </div>
+                    <p class="font-bold text-xl mt-4 mb-2">Email</p>
+                    <input type="text" class="border-[2px] border-white rounded-lg h-12 ps-2" name="email" placeholder="Input your email" />
+                    <p class="font-bold text-xl mt-4 mb-2">Password</p>
+                    <input type="text" class="border-[2px] border-white rounded-lg h-12 ps-2" name="password" placeholder="Password" />
+                    <p class="font-bold text-xl mt-4 mb-2">Confirm Password</p>
+                    <input type="text" class="border-[2px] border-white rounded-lg h-12 ps-2" name="confPassword" placeholder="Confirm Password" />
+                    <button class="mt-16 bg-[#474e9c] text-white font-bold p-2 text-xl rounded-md" name="signUp">Sign Up</button>
+                    <div class="flex mt-4 h-fit mx-auto">
+                        <p class="me-2 text-[#d9dae5]">already have an account?</p>
+                        <p class="text-[#474e9c]">Sign In</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-[50%]">
+                <img src="disney-img.png" alt="" class="mt-16 mb-[30%] h-[45em]" />
+            </div>
+        </div>
+    </body>
 </html>
